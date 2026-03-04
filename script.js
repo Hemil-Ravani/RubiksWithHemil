6a3b5983fe87cc8eafef840edb8aaf267bce7b2f
Hemil Ravani <hemil.ravani@gmail.com>
2026-03-04 21:05:51 +1100

    onsubmit changed

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function onSubmitPressed(){
-    alert("Your form has been submitted.")
+function onSubmitPressed(event) {
+    // Stops the default submission so we can show the alert first
+    event.preventDefault();
+
+    alert("Thank you for booking with us. You will receive an email shortly with all the details.");
+    
+    // Optionally, clear the form
+    document.getElementById("contactForm").reset();
 }
